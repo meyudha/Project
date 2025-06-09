9983b6241d8621641c8e59830c04a3f854d6a843
meyudha <meyudha132@gmail.com>
2025-06-09 16:34:14 +0700

    Update db.php

--- db.php
+++ db.php
@@ -1,9 +1,8 @@
 <?php
-$con = mysqli_connect('db', '123', '123', 'phplogin');
+$con = mysqli_connect('db', '123', '123', 'phplogin'); 
 
 if (!$con) {
     die('❌ Koneksi MySQL gagal: ' . mysqli_connect_error());
 } else {
-    echo '✅ Koneksi MySQL berhasil ke database phplogin';
 }
 ?>
