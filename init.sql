9f673efb4a26c22d07284c4a2505182aed179719
meyudha <meyudha132@gmail.com>
2025-06-09 16:35:46 +0700

    Create init.sql

---
+++ init.sql
@@ -0,0 +1,9 @@
+CREATE DATABASE IF NOT EXISTS phplogin;
+
+USE phplogin;
+
+CREATE TABLE IF NOT EXISTS accounts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL
+);
